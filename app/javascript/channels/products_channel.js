7112ab8ab3acc3233a6cdf1af6cb298028c76bcd
Carlo Santos <rcarlosantos89@gmail.com>
2023-01-17 08:01:55 -0600

    trying to hit received method

--- app/javascript/channels/products_channel.js
+++ app/javascript/channels/products_channel.js
@@ -3,6 +3,7 @@ import consumer from "./consumer"
 consumer.subscriptions.create("ProductsChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    console.log("connection made")
   },
 
   disconnected() {
@@ -11,7 +12,8 @@ consumer.subscriptions.create("ProductsChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    const storeElement = document.querySelector("main.store")
+    console.log("Hello there")
+    const storeElement = document.querySelector("main")
     if (storeElement) {
       storeElement.innerHTML = data.html
     }
